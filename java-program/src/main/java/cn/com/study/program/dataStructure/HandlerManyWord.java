9f6ae179918acc70aadb05e3202cfbc005952878
penggang <1021583229@qq.com>
2018-11-29 00:32:59 +0800

    字典树算法初始化。

---
+++ java-program/src/main/java/cn/com/study/program/dataStructure/HandlerManyWord.java
@@ -0,0 +1,12 @@
+package cn.com.study.program.dataStructure;
+
+/**
+ * 题目：我有500w个单词，你帮忙设计一个数据结构来进行存储，存好之后，我有两个需求。
+ * 1、来了一个新的单词，需要判断是否在这500w个单词中
+ * 2、来了一个单词前缀，给出500w个单词中有多少个单词是该前缀
+ * @author admin
+ *
+ */
+public class HandlerManyWord {
+	
+}
---
+++ java-program/src/main/java/cn/com/study/program/dataStructure/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 主要是数据结构和算法使用实例
+ */
+package cn.com.study.program.dataStructure;
